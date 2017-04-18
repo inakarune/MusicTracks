dbb3e3582a3fa96e5d6e78399ea04646044f5a08
inakarune <inakarune@naver.com>
2017-04-18 20:53:27 +0900

    kkk

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = env => {
         {
           test: /\.js$/,
           exclude: `/node_modules/`,
-          loader: 'babel',
+          loader: 'babel-loader',
           query: {
             presets: ['es2015']
           }
