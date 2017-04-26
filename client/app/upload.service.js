edf43499ddd1dc12bf52fa6b7a25b3c00dde779f
inakarune <inakarune@naver.com>
2017-04-26 20:42:33 +0900

    Update upload.service.js

--- client/app/upload.service.js
+++ client/app/upload.service.js
@@ -1,29 +1,29 @@
 export class uploadService {
-	constructor($http) {
-		'ngInject';
-		this.$http = $http;
-	}
+    constructor($http) {
+        'ngInject';
+        this.$http = $http;
+    }
 
-	insertSong(files){
-		let fileData = new FormData();
-		for(let obj in files){
-			fileData.append('userfile', files[obj]);
-			fileData.append('name', files[obj]);
-		}
+    insertSong(files){
+        let fileData = new FormData();
+        for(let obj in files){
+            fileData.append('userfile', files[obj]);
+            fileData.append('name', files[obj]);
+        }
 
         this.$http.defaults.headers.common['Authorization'] = 'Bearer ' + window.sessionStorage.getItem('accessToken');
-		return this.$http
-                .post('/upload', fileData, {
-            	   transformRequest: angular.identity,
-            	   headers: { 'Content-Type': undefined }
-        		})
-        		.success(response => {
-            	   return response;
-                })
-                .error(() => {
-                    throw new Error('upload Error');
-                });
-	}
+	return this.$http
+	    .post('/upload', fileData, {
+	        transformRequest: angular.identity,
+	        headers: { 'Content-Type': undefined }
+	    })
+	    .success(response => {
+	        return response;
+	    })
+	    .error(() => {
+	        throw new Error('upload Error');
+	    });
+    }
 
     getUploadedSongList(){
         this.$http.defaults.headers.common['Authorization'] = 'Bearer ' + window.sessionStorage.getItem('accessToken');
