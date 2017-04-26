00f5f840323d4a70b80f0af6ae9ef39f1f1b1121
inakarune <inakarune@naver.com>
2017-04-26 20:23:46 +0900

    Update jwt.js

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,26 +2,26 @@ var jwt = require('jsonwebtoken');
 var tokenConfig = require('../config/token');
 var User = require('../models/user');
 
-module.exports = function(req, res, next){
+module.exports = function (req, res, next) {
   var authorizationHeader = req.headers['authorization'];
 
-  if(!authorizationHeader){
+  if (!authorizationHeader) {
     return res.sendStatus(401);
   }
 
   var token = authorizationHeader.split(' ')[1];
 
-  if(token){
-    jwt.verify(token, tokenConfig, function(err, decodedToken){
-      if(err){
+  if (token) {
+    jwt.verify(token, tokenConfig, function (err, decodedToken) {
+      if (err) {
         return res.sendStatus(403);
       }
 
       User.findOne({ email: decodedToken.email })
-        .then(function(userData){
+        .then(function (userData) {
           next();
         })
-        .catch(function(err){
+        .catch(function (err) {
           res.sendStatus(403);
         });
     });
