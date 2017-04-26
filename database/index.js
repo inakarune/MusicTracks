daeadabc8f8e1298ec28638e21a3d24c5c91ed1d
inakarune <inakarune@naver.com>
2017-04-26 20:20:24 +0900

    Update index.js

--- database/index.js
+++ database/index.js
@@ -6,11 +6,11 @@ module.exports = {
   connect(url){
     mongoose.connect(url);
     var db = mongoose.connection;
-    db.on('error', function(err){
+    db.on('error', function (err) {
       console.log('Mongoose default connection error: ' + err);
     });
 
-    db.once('open', function(){
+    db.once('open', function () {
       console.log('Connected to mongodb server');
     });
   }
