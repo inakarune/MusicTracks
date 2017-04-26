2406c27c9648720b3bb015dc9f6881d71f613d86
inakarune <inakarune@naver.com>
2017-04-26 14:18:23 +0900

    code1

--- client/app/search/searchResult.component.js
+++ client/app/search/searchResult.component.js
@@ -39,33 +39,3 @@ export const searchResultComponent = {
     }
   }
 };
-
-// function resultCtrl($scope, $stateParams, searchService, $http){ 
-//   $scope.keyword = $stateParams.keyword;
-  
-//   searchService.searchSong($scope.keyword, 'title')
-//     .then(response => {$scope.searchs = response
-//     });
-
-//   $scope.imgObj = {
-//     "width" : "150px",
-//     "margin-top" : '20px',
-//     "margin-right" : '20px',
-//     "margin-left": '20px'
-//   };
-
-//   $scope.playNg = function(index){
-//     var idx = index.toString();
-//     var audio = document.getElementById(idx);
-//     audio.load();
-//     audio.play();
-//   };
-
-//   $scope.sendCart = function(title, artist, album, price){
-//     return $http({
-//       method: 'GET',
-//       url: '/cart/:item',
-//       params: {'title' : title, 'artist': artist, 'album': album, 'price': price }
-//     });
-//   };
-// }
