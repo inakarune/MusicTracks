54a6388bc16109ff68849001716a8eba26cff6b3
inakarune <inakarune@naver.com>
2017-04-26 20:11:09 +0900

    Delete config.js

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleID : '685252868584-i831fvo947s3sns7obu8o5e9800et1ta.apps.googleusercontent.com',
-  googleSecret : 'k9deyuoIjS-djpM8XlE6Hc0R',
-  apiKey : '1600254710099986',
-  secret : 'uDBzswGA7JFlga8qado79dKLD5mWzWG3zhD2OjDW2ihjtqKDD8LMmvyZAUWaQCdqUQsJa6FcoXwRngv1',
-  keyCode: 'imp64353457'
-};
